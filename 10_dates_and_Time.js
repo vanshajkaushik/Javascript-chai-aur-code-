2542eae874688c00729ef4567608b4729e73f0c4
vanshaj <apple@vanshajs-macbook-air.local>
2024-12-29 01:44:51 +0530

    here we studied about the dates and time

---
+++ 10_dates_and_Time.js
@@ -0,0 +1,9 @@
+// Dates 
+
+let mydate = new Date()
+console.log(mydate); // here the output comes in ms from the time it started which is complex
+console.log(mydate.toString()); // there are similar different other methods 
+
+//date is object 
+//date too has different methods which can be studied or have reference to the video
+
