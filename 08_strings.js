367e1d185b80693418be6f69cdb4dfbdd6004432
vanshaj <apple@vanshajs-macbook-air.local>
2024-12-29 01:07:25 +0530

    here in this we studied a brief about strings

---
+++ 08_strings.js
@@ -0,0 +1,12 @@
+//we can give any variable a string value 
+
+var name = "vanshaj"
+
+console.log(`my name is ${name}`) // this is a way in which we can print a lot of values 
+
+// another way to declare a string is 
+
+const gamename =  new String('vanshaj') //make sure the S in String is capital
+console.log(gamename) // here the string is not an array it just stores each value in key value pair format 
+
+// later we can copy this format in the console of our browser and can find different other features (can have reference from the video)
