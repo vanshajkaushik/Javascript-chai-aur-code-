9a5b1d469832c149b0d47d795f7b2ec9693d470c
vanshaj <apple@vanshajs-macbook-air.local>
2024-12-28 21:38:58 +0530

    in this video we studied about the special conversion cases

---
+++ 04_string_to_number_conversion.js
@@ -0,0 +1,13 @@
+let value = 33 
+
+let newvalue = - value 
+//console.log(newvalue)
+
+var str1 = "hello"
+var str2 = "vanshaj"
+console.log(str1+str2)
+console.log("hello"+"kaushik")
+console.log(1+2+"2") //special cases please refer to video
+console.log("1"+2+2) //special cases please refer to video
+
+console.log(+true) //special cases please refer to video
