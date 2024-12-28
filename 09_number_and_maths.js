be49df01251a02d56c46819222eb365907249cfd
vanshaj <apple@vanshajs-macbook-air.local>
2024-12-29 01:30:51 +0530

    here we learned about the math library and the concept of numbers

---
+++ 09_number_and_maths.js
@@ -0,0 +1,12 @@
+// we can similar to strings declare numbers in two different ways 
+
+var score = 100
+console.log(score);
+
+
+const newscore = new Number(100)
+console.log(newscore); //here you can see the difference its very similar to what we saw in the strings 
+// if we copy this code in the console of our browser we will get different properties as we did in strings which can be studied 
+// from cdn's
+
+//for the math library which comes with js we can go on console in our browser and see different properties
