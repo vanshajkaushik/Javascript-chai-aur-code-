5c05590be339f14ab0cd71aa654122ad0541d303
vanshaj <apple@vanshajs-macbook-air.local>
2024-12-29 23:53:33 +0530

    in this we have discussed about arrays

---
+++ 11_arrays.js
@@ -0,0 +1,15 @@
+//arrays in js are resizable and can be declared as 
+
+var myarr = [1,2,3,4,5]
+
+console.log(myarr[2]);
+
+
+//they can store single type or multiple type data
+
+var newarr = [1 ,2,"vanshaj"]
+
+console.log(newarr[2]);
+
+// similar to strings and dates there are different methods in arrays which can be seen in prototype 
+// by going in console 
