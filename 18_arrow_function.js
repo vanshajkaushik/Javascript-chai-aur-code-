ef521ed321dcf1299aa2b2549b85c3f788dceb56
vanshaj <apple@vanshajs-macbook-air.local>
2025-01-05 01:49:18 +0530

    in this we learned the concept of arrow function and its syntax

---
+++ 18_arrow_function.js
@@ -0,0 +1,14 @@
+// its is just a new method to make a function
+
+const chai = () => {
+    console.log("hello");
+    
+}
+
+chai()
+
+// Implicit return arrow function
+
+const addtwo = (num1,num2) => (num1+num2) // if we use ( ) no need to return but if we use { } we need to write return 
+
+console.log(addtwo(5,6))
