9ab767af5fb51ec07e46e3a99caa753e05831a8c
vanshaj <apple@vanshajs-macbook-air.local>
2025-01-06 02:44:25 +0530

    here we studied about filter and reduce properties

---
+++ 24_filter_maps_and_reduce.js
@@ -0,0 +1,10 @@
+//filter in maps 
+const mynums = [1,2,3,4,5,6,7,8,9]
+
+const newnums = mynums.filter((num)=>{
+    return num > 4
+}) 
+
+console.log(newnums);
+// To do the same thing using filter we would have to use if condition that would work too  
+//for reduce please refer to the video - for better understanding go to last 10 mins..
