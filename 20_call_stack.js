acf8dbe7b279e76aaeeec6c6b76ab1a1471c18ea
vanshaj <apple@vanshajs-macbook-air.local>
2025-01-05 16:58:19 +0530

    in this we discussed about call stack

---
+++ 20_call_stack.js
@@ -0,0 +1,7 @@
+//When we say JavaScript is single-threaded, 
+//it means JavaScript executes code in a single sequence of instructions, 
+//one step at a time, in a single thread of execution.
+
+//++++++++++++++++++++++++IMPORTANT++++++++++++++++++++//
+
+// For call stack please refer to chai aur code video only...
