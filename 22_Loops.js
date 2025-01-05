3a9a5b1128a2586a4d044c859d46b64011ecc798
vanshaj <apple@vanshajs-macbook-air.local>
2025-01-06 01:15:35 +0530

    in this we have studied about loops

---
+++ 22_Loops.js
@@ -0,0 +1,24 @@
+//FOR LOOPS
+
+for(let i = 1;i<=10;i++){
+console.log(i);
+
+}
+
+// break and continue : break gets you out of the scope whereas continue escapes the code for once but does not kick you out of the scope 
+
+//While Loops 
+
+let i = 0
+while(i<10){
+    console.log("hello");
+    i++
+}
+
+//Do while Loops 
+let index =0
+do {
+    console.log("do");
+    index++
+    
+} while (index<3);
