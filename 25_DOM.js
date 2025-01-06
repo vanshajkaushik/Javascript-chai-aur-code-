ef8ff0c1443cdd2dac221f419773e4cae692b70a
vanshaj <apple@vanshajs-macbook-air.local>
2025-01-06 23:01:56 +0530

    we discussed about dom

---
+++ 25_DOM.js
@@ -0,0 +1,11 @@
+//DOM - Document Object Model - here i have written notes only major work was done on the browser console itself review the video for better understanding
+// 1) getelementbyid('id')
+// we use document.getelementbyid - here document selects the entire page and then we can select the way we want to do DOM 
+//queryselector and queryselectorall - first gives only the first value whereas the second gives all the value 
+//htmlcollection and nodelist are similar to array but are not actually array to perform the array task we need to convert them to array 
+//store the html collection/nodelist in a variable and then 
+// Array.from("the variable name ") - this would convert it into array and then we could perform that task 
+//myConvertedArray. forEach (function (li){ :foreach is a method usses function as we learnt before 
+//li.style.color = 'orange'
+//})
+//to create an element we use document.createelement("element name")
