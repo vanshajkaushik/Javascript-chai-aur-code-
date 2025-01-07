c98ed2bd3328a9d7dac47a461cfafa6f2d696e3c
vanshaj <apple@vanshajs-macbook-air.local>
2025-01-07 23:01:44 +0530

    in this we finally ended the js series

---
+++ 29_Prototype.js
@@ -0,0 +1,8 @@
+// Javascript has a prototype behavior - that is if it doesnt find anything it would go to the parent ,
+// grandparent , greatgrandparent untill it gets the value as null - we see this when we open console on browser we see the prototype underneath /
+
+//++++++++++++++++++++++IMPORTANT+++++++++++++++++++++++++++
+
+// The videos after Fetch have been a bit theortical so for such advance topics please refer to the videos 
+
+// Note to myself - watch about prototype once gained experience in javascript 
